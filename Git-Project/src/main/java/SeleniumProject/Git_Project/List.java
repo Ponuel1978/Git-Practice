b4991f8dfb75948dfcfbda1bbca590c329f0766e
Ponuel <Ponuel@10.0.0.72>
2020-05-20 18:23:40 -0400

    version1

---
+++ Git-Project/src/main/java/SeleniumProject/Git_Project/List.java
@@ -0,0 +1,19 @@
+package SeleniumProject.Git_Project;
+
+import java.util.ArrayList;
+
+public class List {
+
+	public static void main(String[] args) {
+		ArrayList <String> list=new ArrayList<String>();
+		list.add("Hunny");
+		list.add("Munny");
+		list.add("Sunny");
+		list.add("Punny");
+		list.add("Hunny");
+		System.out.println(list);//total size
+		
+
+	}
+
+}
